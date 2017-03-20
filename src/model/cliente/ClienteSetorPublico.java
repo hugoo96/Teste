cd7306406a5499a1755e7aee28ca405bec36d020
Hugo <hugoo_96@hotmail.com>
2017-03-20 00:36:13 -0300

    Consertando

---
+++ src/model/cliente/ClienteSetorPublico.java
@@ -0,0 +1,26 @@
+package model.cliente;
+
+
+public class ClienteSetorPublico extends Cliente {
+	private String nivelOrgao;//municipal,estadual,federal
+	
+	public ClienteSetorPublico(String nome, String telefone, String email, String cnpj, String nivelOrgao) {
+		super(nome, telefone, email, cnpj);
+		this.nivelOrgao = nivelOrgao;
+	}
+
+	public String getNivelOrgao() {
+		return nivelOrgao;
+	}
+
+	public void setNivelOrgao(String nivelOrgão) {
+		this.nivelOrgao = nivelOrgao;
+	}
+
+	@Override
+	public String toString() {
+		return "Nome: " + nome + "\nTelefone: " + telefone + "\nEmail=" + email + "\nCnpj: "+cnpj+"\nNivel do orgao: "+nivelOrgao;
+	}
+	
+	
+}
